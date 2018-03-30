fbe696f3f42d89962b09a9ca7a72e02629993c1f
rodricksnyc <rodricksnyc@gmail.com>
2018-03-29 21:52:52 -0400

    changes

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
+$(document).ready(function() {
+  // $('#newMovies').hide();
+  // $('#submit').on('click', function(){
+  //   $('#newMovies').show();
+  //
+  // });
+
+  $('')
+
+  $('#newMovies').on('click', function() {
+    $('#slide').hide();
+    $('#result').append();
+
+  });
+
+
 var button = document.querySelector('button')
 var resultContainer = document.querySelector('#result')
 button.addEventListener('click', function( ) {
-  let movieName = $('#movie').val();
+let movieName = $('#movie').val();
 
 
   $.ajax('http://api.themoviedb.org/3/movie/upcoming?api_key=6af6582e4eb5ba81b0db6b8c582d67a4&query='+
-
+//new api will have api + zipcode (from firebase object)
 
    movieName)
     .done(function(response) {
@@ -20,7 +36,19 @@ button.addEventListener('click', function( ) {
         let overview = movie.overview;
         let poster =movie.poster_path;
         console.log(movie);
-        $('#result').append('<img src="http://image.tmdb.org/t/p/w342// + poster">' + '<li><h3>'+ title + vote + '</h3><h1><strong>Overview: </strong> ' + overview + '</h1></li>');
+
+        var newdiv = $("<div>")
+          .addClass("col-lg-6 col-md-6 col-sm-6")
+
+        var newhtml = '<img src=http://image.tmdb.org/t/p/w342//' + poster + '>' + '<h4>'+ title +" "+""+ vote + '</h4>'
+        newdiv.append(newhtml)
+
+        $('#result').append(newdiv);
+      //   var button = $('<button>', {
+      //     text: "Click to Find this Movie",
+      //     class: 'locationSearch'
+      //   })
+      //   $('#result').append(button);
       }
       console.log(response);
     })
@@ -28,3 +56,6 @@ button.addEventListener('click', function( ) {
       console.log(error);
     });
 })
+
+
+});
