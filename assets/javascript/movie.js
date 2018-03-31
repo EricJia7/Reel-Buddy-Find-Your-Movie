e4f9957b2bc8e2f64b7e8ad86037e69d804a5d25
Eric J <34559118+jiapurdue@users.noreply.github.com>
2018-03-31 00:05:19 -0400

    add upcoming, current playing, most popular api call and display the result on webpage, create get youtube id function, add modal to the page for viewing Trailer

--- assets/javascript/movie.js
+++ assets/javascript/movie.js
@@ -1,38 +1,63 @@
-
 var tmdbKey = "?api_key=6af6582e4eb5ba81b0db6b8c582d67a4";
 var selRegion = "&region=US";
 var selLanguage = "&language=en-US";
 var selType = "&with_release_type=3";
 var selSort = "&sort_by=popularity.desc";
-var selPage = "&page=1";
+var selPage1 = "&page=1";
+var selPage2 = "&page=1";
+
 var selTotalPages = "&total_pages=10";
 var selTotalResults = "&total_results=40";
 
 var usMvCompanies = ["Paramount", "Universal Pictures", "Legendary Pictures", "Warner Bros. Pictures", "Sony Pictures", "Sony Pictures Animation"]
 
-var tmdbComing = "http://api.themoviedb.org/3/movie/upcoming" + tmdbKey + selLanguage + selType + selSort + selPage;
+// URL for movies currently show in theater. 
+var tmdbComing = "http://api.themoviedb.org/3/movie/upcoming" + tmdbKey + selLanguage + selType + selSort + selPage1;
 
-var tmdbNowPlay = "http://api.themoviedb.org/3/movie/now_playing" + tmdbKey + selLanguage + selType + selSort + selPage;
+//URL for movies recently put off from the market. 
+var tmdbNowPlay = "http://api.themoviedb.org/3/movie/now_playing" + tmdbKey + selLanguage + selType + selSort + selPage1;
+
+//URL for most poplular movies. 
+var tmdbPopular =  "https://api.themoviedb.org/3/movie/popular" + tmdbKey + selLanguage + selPage2;
+
+//URL for single movie
+var singleMv = "https://api.themoviedb.org/3/movie/";
 
 var selMovies = [];
 
 var responseTmdb = {};
 
-var singleMv = "https://api.themoviedb.org/3/movie/";
-
 var movieIdList = [];
 var movieIdListBl = [];
 
+var selMvYt = "";
+
+// Firebase read and write
+var config = {
+  apiKey: "AIzaSyDTn3DEHWxDZeOEk7RIhVgl9BvvL49hFxU",
+  authDomain: "find-your-movie.firebaseapp.com",
+  databaseURL: "https://find-your-movie.firebaseio.com",
+  projectId: "find-your-movie",
+  storageBucket: "",
+  messagingSenderId: "87493737030"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
 //Function to get Youtube URL for selected movie ID
-function tmdbYTUrlKey(movieId) {
+function tmdbYTUrlKey(movieId,id) {
+  selMvYt = "";
   var urlYt =  "http://api.themoviedb.org/3/movie/" + movieId + "/videos" + tmdbKey;
   $.ajax({
     url: urlYt,
     method: "GET"
   }).done(function(response){
-    console.log("Here is youtube id ajax response: " + response);
     var results = response.results;
-    console.log(results[0].key);
+    selMvYt = results[0].key;
+    console.log(selMvYt);
+    addModal(id);
   }).fail(function(error) {
     console.log(error);
   });
@@ -45,9 +70,8 @@ function mvGroupQuery(url) {
     url:urlInputGroup,
     method: "GET",
   }).done(function(response){
-    console.log(response);
     var results = response.results;
-    responseTmdb = response.results
+    responseTmdb = response.results;
     for(var i = 0; i<results.length; i++) {
       var id = results[i].id;
       movieIdList.push(id);
@@ -60,8 +84,7 @@ function mvGroupQuery(url) {
         "overview": results[i].overview,
       };
       selMovies.push(newObj);
-      addMv2Page(newObj);
-      console.log(newObj);
+      addMvContainer(newObj);
     };
   }).fail(function(error) {
     console.log(error);
@@ -77,7 +100,6 @@ function mvSingleQuery(id) {
     url:urlInputSingle,
     method: "GET",
   }).done(function(response){
-    console.log(response);
     var result = response;
     var prdCompanies = result.production_companies;
     var knownCompany = false;
@@ -87,9 +109,9 @@ function mvSingleQuery(id) {
         knownCompany = true;
       };
     if(knownCompany){
-      console.log("here is a " + knownCompany + " one")
+      console.log("here is a " + knownCompany + " one");
     } else {
-      console.log("NO~~~~~~~~~~~~")
+      console.log("Here is a false one");
     };
     });
   }).fail(function(error) {
@@ -97,48 +119,107 @@ function mvSingleQuery(id) {
   });
 };
 
-function addMv2Page(arr) {
-    var newdcol = $("<div>").addClass("col-lg-4 col-md-4 col-sm-4");
-    var newrow1 = $("<div>").addClass("row");
-    var newrow2 = $("<div>").addClass("row");
-    var newcol1 = $("<div>").addClass("col-lg-12 col-md-12 col-sm-12");
-    var newcol2_1 = $("<div>").addClass("col-lg-8 col-md-8 col-sm-8 text-center");
-    var newcol2_2 = $("<div>").addClass("col-lg-4 col-md-4 col-sm-4 text-center");
+function addMvContainer(arr) {
 
-    var img = '<img src='+ arr["posterUrl"] +">";
-    console.log(img);
-    var title1 = '<h4>'+ arr["name"] + '</h4>';
-    var title2 = '<h4>' + arr["vote"] + '</h4>' 
+  $("#movie-slide").hide();
+  
+  //newdcol will display the mv image, title and rating, call it the trailer video container
+  var newdcol = $("<div>").addClass("col-lg-4 col-md-4 col-sm-4");
+  //img show in this md-12 row
+  var newrow1 = $("<div>").addClass("row");
+  //text(title,rating) show here
+  var newrow2 = $("<div>").addClass("row");
 
-    newcol2_1.append(title1)
-    newcol2_2.append(title2)
-    newcol1.append(img);
+  var newcol1_left = $("<div>").addClass("col-lg-1 col-md-1col-sm-1");
+  var newcol1_right= $("<div>").addClass("col-lg-1 col-md-1 col-sm-1");
+  var newcol1 = $("<div>").addClass("col-lg-10 col-md-10 col-sm-10");
 
-    newrow1.append(newcol1);
-    newrow2.append(newcol2_1);
-    newrow2.append(newcol2_2);
+  var newcol2_1_left = $("<div>").addClass("col-lg-1 col-md-1 col-sm-1 text-center");
+  var newcol2_1 = $("<div>").addClass("col-lg-8 col-md-8 col-sm-8 text-center");
+  var newcol2_2 = $("<div>").addClass("col-lg-2 col-md-2 col-sm-2 text-center");
+  var newcol2_2_right = $("<div>").addClass("col-lg-1 col-md-1 col-sm-1 text-center");
 
-    newdcol.append(newrow1);
-    newdcol.append(newrow2);
+  var newImg = $("<img>")
+    .attr("src",arr["posterUrl"])
+    .addClass("img-fluid");
+  newcol1.append(newImg);
+  newrow1.append(newcol1_left);
+  newrow1.append(newcol1);
+  newrow1.append(newcol1_right);
 
-    newdcol.attr("id", arr["id"]);
+  var newp1 = $("<p>")
+    .addClass("mvTitle text-center")
+    .text(arr["name"]);
+  var newp2 = $("<p>")
+    .addClass("mvTitle text-center")
+    .text(arr["vote"]);
 
-    $("#movieContainer").append(newdcol);
-    console.log("~~~~~~~~~~~~~~~");
+  newcol2_1.append(newp1);
+  newcol2_2.append(newp2);
+
+  newrow2.append(newcol2_1_left).append(newcol2_1).append(newcol2_2).append(newcol2_2_right);
+
+  newdcol.append(newrow1);
+  newdcol.append(newrow2);
+  //add movie id to the movie display container
+  newdcol.attr("id", arr["id"])
+    .addClass("trailer-video-container")
+    .attr("data-toggle", "modal")
+    .attr("data-target", "#"+arr["id"]+"Modal");
+
+  // $("#movieContainer").append(newdcol);
+  $("#movieContainer").show("slow", function() {
+    $(this).append(newdcol)
+  });
+
 };
 
+function addModal(str) {
+  console.log("!!!!!!!!!!!!!" + str);
+  $(".modalIndicator").attr("id",str);
+  console.log("!!!!!!!!!!!!!")
+  console.log($(".modal .fade .modalIndicator"));
+};
 
 $("#inTheaterBtn").click(function(event){
+  $("#movieContainer").empty();
+  // $("#movieContainer").css("background-color", "white" );
   event.preventDefault();
   selMovies = [];
   mvGroupQuery(tmdbComing);
+  $(".trailer-video-container").on("click", ".img-fluid",function(event){
+    alert("ha")
+  });
 });
 
 $("#pastMovieBtn").click(function(event){
+  $("#movieContainer").empty();
+  // $("#movieContainer").css("background-color", "white" );
   event.preventDefault();
   selMovies = [];
   mvGroupQuery(tmdbNowPlay);
 });
 
+$("#mostPopularBtn").click(function(event){
+  $("#movieContainer").empty();
+  // $("#movieContainer").css("background-color", "white" );
+  event.preventDefault();
+  selMovies = [];
+  mvGroupQuery(tmdbPopular);
+});
+
+$(document).on("click", ".trailer-video-container", function(event){
+  var mvId = $(this).attr("id");
+  var dataTarget = $(this).attr("data-target").replace("#","");
+  console.log(mvId, dataTarget);
+  tmdbYTUrlKey(mvId,dataTarget);
+});
       
+function addMvSlide(arr) {
+  $("#movieContainer").empty();
+  var imgSlide = $("<img>")
+    .addClass("carousel-image")
+    .attr("src",arr["posterUrl"])
+  $("#movie-slide").append(imgSlide)
+};
 
